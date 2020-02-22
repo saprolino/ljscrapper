37666f1a657b5859f80e1f8a8cae7c5233d57ae2
saprolino <saprolino@Gmail.com>
2020-02-22 15:16:01 +0300

    Adding to main

---
+++ ljclass.py
@@ -0,0 +1,113 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+# https://www.livejournal.com/go.bml?journal={}&itemid={}}&dir=prev
+# https://www.livejournal.com/go.bml?journal={}&itemid={}}&dir=next
+
+class Blog:
+    def __init__(self, blogname=None):
+        self.blogname = "" if blogname == None else blogname
+
+        self.articles = dict()
+        # self.url_lj_template = "https://{}.livejournal.com/{}.html"
+        # self.url_domain_template = "https://{}/{}.html"
+        self.url_template = "https://{}.livejournal.com/{}.html"
+        self.ssl_enabled = False if "." in self.blogname else True
+        self.url_prev_template = "https://www.livejournal.com/go.bml?journal={}&itemid={}&dir=prev"
+        self.url_next_template = "https://www.livejournal.com/go.bml?journal={}&itemid={}&dir=next"
+        self.shift = 16 + len(self.blogname)
+
+    # self.shift = 1
+
+    def parse(self, id):
+        cookies_jar = requests.cookies.RequestsCookieJar()
+        cookies_jar.set('prop_opt_readability', "1", domain='.livejournal.com', path='/')
+        cookies_jar.set('adult_explicit', "1", domain='livejournal.com', path='/')
+        # the_url = self.url_domain_template.format(self.blogname, id) if "." in self.blogname else self.url_lj_template.format(self.blogname, id)
+        # print(the_url)
+        # input()
+        the_url = self.url_template.format(self.blogname, id)
+        page = requests.get(the_url, cookies=cookies_jar, verify=self.ssl_enabled)
+        data = page.text
+        soup = BeautifulSoup(data, features="html.parser")
+        title = soup.title.string[:-self.shift].rstrip()
+        date = soup.findAll("time", {"class": "b-singlepost-author-date published dt-published"})
+        date = date[0].getText()
+        tags = soup.findAll("span", {"class": "b-singlepost-tags-items"})
+        if len(tags):
+            tags = list(map(str.strip, tags[0].getText().split(", ")))
+        else:
+            tags = []
+        self.articles[str(id)] = {"title": title, "date": date, "tags": tags}
+        return "{} {} {} {}".format(id, date, title, tags)
+
+    def getPreviousId(self, id):
+        requestUrl = self.url_prev_template.format(self.blogname, id)
+        page = requests.get(requestUrl, verify=self.ssl_enabled)
+        if page.url == requestUrl:
+            return 0
+        else:
+            return page.url.split("/")[-1][:-5]
+
+    def getNextId(self, id):
+        requestUrl = self.url_next_template.format(self.blogname, id)
+        page = requests.get(requestUrl, verify=self.ssl_enabled)
+        if page.url == requestUrl:
+            return 0
+        else:
+            return page.url.split("/")[-1][:-5]
+
+    def retrieveFromOldest(self, id, up_to=-1):
+        current_id = id
+        prev_id = self.getPreviousId(current_id)
+        while current_id and up_to:
+            page = self.parse(current_id)
+            current_id, prev_id = prev_id, self.getPreviousId(prev_id)
+            print(page)
+            up_to -= 1
+
+    def getState(self):
+        print("There are {} articles in your data".format(len(self.articles)))
+
+    def retrieveFromNewest(self, id, up_to=-1):
+        current_id = id
+        next_id = self.getNextId(current_id)
+        while current_id and up_to:
+            page = self.parse(current_id)
+            current_id, next_id = next_id, self.getNextId(next_id)
+            print(page)
+            up_to -= 1
+
+    def saveToFile(self, filename):
+        with open(filename, 'w', encoding='utf8') as fp:
+            json.dump({"blogname": self.blogname, "data": self.articles}, fp, ensure_ascii=False, sort_keys=True,
+                      indent=4)
+
+    def readFromFile(self, filename):
+        with open(filename, 'r', encoding='utf8') as fp:
+            data = json.load(fp)
+            self.blogname, self.articles = data["blogname"], data["data"]
+            self.ssl_enabled = False if "." in self.blogname else True
+            self.shift = 16 + len(self.blogname)
+
+    def anyNewer(self):
+        if len(self.articles) == 0:
+            return True
+        newest = max(self.articles, key=lambda i: self.articles[i]["date"])
+        return bool(self.getNextId(newest))
+
+    def anyOlder(self):
+        if len(self.articles) == 0:
+            return True
+        oldest = min(self.articles, key=lambda i: self.articles[i]["date"])
+        return bool(self.getPreviousId(oldest))
+
+    def retrieveDown(self, up_to=-1):  # takes the oldest article in self.articles and continues retrieving
+        current_id = min(self.articles, key=lambda i: self.articles[i]["date"])
+        self.retrieveFromOldest(current_id, up_to)
+
+    def retrieveUp(self, up_to=-1):  # takes the oldest article in self.articles and continues retrieving
+        current_id = max(self.articles, key=lambda i: self.articles[i]["date"])
+        self.retrieveFromNewest(current_id, up_to)
---
+++ main.py
@@ -0,0 +1,63 @@
+import requests
+from bs4 import BeautifulSoup
+
+from ljclass import *
+import sys
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+blogname = input("Input blogname: ")
+# blogname = "livejournal"
+
+# url_lj_template = "https://{}.livejournal.com/"
+# url_domain_template = "https://{}/"
+url_template = "https://{}.livejournal.com/"
+the_url = url_template.format(blogname)
+x = requests.get(the_url, verify=False).status_code
+
+if x == 404:
+	print("This blog doesn't exist.")
+	sys.exit()
+else:
+	print("This blog exists...")
+
+chunk_size = 10
+if len(sys.argv) == 2:
+	chunk_size = int(sys.argv[1])
+
+directory = "data"
+ext = ".json"
+
+location = directory + "/" + blogname + ext
+
+try:
+	f = open(location)
+	f.close()
+	print("Using existing file...")
+	a = Blog()
+except FileNotFoundError:
+	print("File doesn't exist, making a new one ({})...".format(location))
+	id = input("Input some article's id: ")
+	a = Blog(blogname)
+	a.retrieveFromNewest(id, 1)
+	a.saveToFile(location)
+
+a.readFromFile(location)
+
+#52504
+
+while a.anyOlder():
+	a.retrieveDown(chunk_size)
+	a.saveToFile(location)
+
+while a.anyNewer():
+	a.retrieveUp(chunk_size)
+	a.saveToFile(location)
+
+a.getState()
+#1008373
+
+#print(a.articles)
+#a.readFromFile("result.json")
+#print(a.articles)
