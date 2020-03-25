f4843cda6b827350b7a5537a80850b79bf35cb71
saprolino <saprolino@Gmail.com>
2020-03-25 21:38:44 +0300

    Deleted HTML

--- htmlclass.py
+++ htmlclass.py
@@ -5,7 +5,7 @@ class HTML:
     def __init__(self, blogname):
         with open("data/"+blogname+".json", 'r', encoding='utf8') as fp:
             self.data = json.load(fp)
-
+            
     def saveHTML(self):
         templateLoader = jinja2.FileSystemLoader(searchpath="./")
         templateEnv = jinja2.Environment(loader=templateLoader)
